b00e0b78728fe89050f5c92b0fdae5eafec66534
C0DESbyKeshav <keshavmandal2503@gmail.com>
2025-02-03 00:16:41 +0530

    Py

---
+++ CWH Crash/Chapter 5 (Dictionary and Sets).py/Problem Set/Problem 1.py
@@ -0,0 +1,14 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+dict = {"Pustak": "Book",
+        "Krodh": "Anger",
+        "Sehensheelta": "Endurance",
+        "Stheerta": "Patience",
+        "Shishtachaar": "Manners",
+        "Yojna": "Plan",
+        "Parvat": "Mountain",
+        "Anubhav": "Experience",
+        "Gyaan": "Knowledge"}
+
+word = input("Woh shabd likho jiska aapko matlab jaan naa hai : ")
+
+print(dict[word])
