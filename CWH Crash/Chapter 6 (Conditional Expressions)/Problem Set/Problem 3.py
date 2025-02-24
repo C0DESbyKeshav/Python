fa66c2a8b3ffe5d45852410af79572f237d73a0b
Keshav Mandal <keshavmandal2503@gmail.com>
2025-02-24 15:06:38 +0530

    Update Problem Set

---
+++ CWH Crash/Chapter 6 (Conditional Expressions)/Problem Set/Problem 3.py
@@ -0,0 +1,5 @@
+'''A spam comment is defined as a text containing following keywords:
+"make a lot of money", "buy now", "subscribe this", "click this"
+Write a program to detect this spams.'''
+
+comment = input("Enter a comment:\n")
