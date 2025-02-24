ed6eb3698dd7849e94f40474b38af3e222bf1ba0
Keshav Mandal <keshavmandal2503@gmail.com>
2025-02-24 15:20:26 +0530

    Update Problem 3.py

--- CWH Crash/Chapter 6 (Conditional Expressions)/Problem Set/Problem 3.py
+++ CWH Crash/Chapter 6 (Conditional Expressions)/Problem Set/Problem 3.py
@@ -3,3 +3,13 @@
 Write a program to detect this spams.'''
 
 comment = input("Enter a comment:\n")
+
+s1 = "make a lot of money"
+s2 = "buy now"
+s3 = "subscribe this"
+s4 = "click this"
+
+if((s1 in comment) || (s2 in comment) || (s3 in comment) || (s4 in comment)):
+	print("You entered a spam comment.")
+else:
+	print("You entered a safe comment.")
