7763910ad934d34b520c7f3663dec1c6d7c5a297
Keshav Mandal <keshavmandal2503@gmail.com>
2025-02-26 23:55:36 +0530

    Delete CWH Crash/Chapter 6 (Conditional Expressions)/Problem Set directory

--- CWH Crash/Chapter 6 (Conditional Expressions)/Problem Set/Problem 4.py
+++
@@ -1,7 +0,0 @@
-# Write a program to check whether a given username contains less than 10 characters or not.
-
-username = input("Enter a username: ")
-if (len(username) >= 10):
-    print(f"The entered username '{username}' is not less than 10 characters.")
-else:
-  print(f"The entered username '{username}' is less than 10 characters.")
--- CWH Crash/Chapter 6 (Conditional Expressions)/Problem Set/Problem 5.py
+++
@@ -1,2 +0,0 @@
-# Write a program which finds out whether a given name is present in the list or not.
-
